423f8e313e205b4e30f7cea009404f5b2fad8b30
Earl Sabalo <58910880+0xearl@users.noreply.github.com>
2020-05-03 16:31:09 +0800

    Update pyfuzzer.py

--- pyfuzzer.py
+++ pyfuzzer.py
@@ -4,7 +4,7 @@ import urllib3
 from socket import gethostbyname, gaierror
 from anytree import Node, RenderTree
 import argparse
-
+import os
 urllib3.disable_warnings()
 s = requests.Session()
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"}
@@ -14,9 +14,16 @@ not_allowed_list = []
 not_found_list = []
 class fuzz():
 	  parser = argparse.ArgumentParser()
+	  parser.add_argument('-help', help='Usage:python pyfuzzer.py -url https://target.com')
 	  parser.add_argument('-url', help='https://target.com')
+	  parser.add_argument('-wordlist', help='custom wordlist ex: /path/to/wordlist.txt')
 	  args = parser.parse_args()
 	  url = args.url
+	  if args.wordlist == None:
+	  	wordlist = '{}\\payload.txt'.format(os.getcwd())
+	  else:
+	  	wordlist = args.wordlist
+
 	  def checkRobots(url):
 	   	r = s.get(url+"robots.txt", verify=False, timeout=5, headers=header)
 	   	if r.status_code == requests.codes.ok:
@@ -29,9 +36,9 @@ class fuzz():
 	   	else:
 	   		print("Robots.txt wasn't found proceeding to bruteforcing files/dirs")
 	   		pass
-	  def dirhunt(url):
+	  def dirhunt(url, wordlist):
 	  	try:
-	  		for link in open("payload.txt").read().splitlines():
+	  		for link in open(wordlist).read().splitlines():
 	  			print("Please Wait..", end='\r')
 		  		r = s.get(url+link, verify=False, timeout=5, headers=header)
 		  		if r.status_code == requests.codes.ok:
@@ -53,28 +60,28 @@ class fuzz():
 	  def _tree(found_list, redirects_list, not_allowed_list, not_found_list):
 	  	not_found = Node('Not Found')
 	  	for url in not_found_list:
-	  		url = Node(args.url, parent=not_found)
+	  		found_url = Node(url, parent=not_found)
 	  	for pre, fill, node in RenderTree(not_found):
 	  		print('%s%s' % (pre, node.name))
 	  	found = Node('Found')
 	  	not_allowed = Node('Not Allowed')
 	  	for url in not_allowed_list:
-	  		found_url = Node(args.url, parent=not_allowed)
+	  		found_url = Node(url, parent=not_allowed)
 	  	for pre, fill, node in RenderTree(not_allowed):
 	  		print('%s%s' % (pre, node.name))
 	  	redirects = Node('Redirects')
-	  	for args.url in redirects_list:
-	  		found_url = Node(args.url, parent=redirects)
+	  	for url in redirects_list:
+	  		found_url = Node(url, parent=redirects)
 	  	for pre, fill, node in RenderTree(redirects):
 	  		print('%s%s' % (pre, node.name))
 	  	for url in found_list:
-	  		found_url = Node(args.url, parent=found)
+	  		found_url = Node(url, parent=found)
 	  	for pre, fill, node in RenderTree(found):
 	  		print('%s%s' % (pre, node.name))
-	  	checkRobots(url)
-	  	dirhunt(url)
-	  	print('\n')
-	  	_tree(found_list, redirects_list, not_allowed_list, not_found_list)
+	  checkRobots(url)
+	  dirhunt(url, wordlist)
+	  print('\n')
+	  _tree(found_list, redirects_list, not_allowed_list, not_found_list)
 
 if __name__ == '__main__':
 	fuzz()
