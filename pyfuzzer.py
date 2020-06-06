41a64d2a36527aacfd1626ad1318e7d1f89b3a5d
0xearl <earljohnsabalo@gmail.com>
2020-06-06 22:02:20 +0800

    Ui and Algorithm Changes

--- pyfuzzer.py
+++ pyfuzzer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-import requests, re, urllib3, argparse, os, logging
-import socket
-import os
-
+import requests, re, urllib3, argparse, os, logging, time, socket
 
 try:
     from colorama import init
@@ -31,22 +28,26 @@ logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 terminal_size = os.get_terminal_size().columns
 
 def checkRobots(url):
-	r = session.get(url+'robots.txt', verify=False)
+	r = session.get(url+'robots.txt', verify=False, headers=header)
 	if r.status_code == requests.codes.ok:
 		lines = r.text.splitlines()
-		for keyword in lines:
-			if re.search('admin|api|controll_panel|users|usarios|administrator|json|login|php|uploads|manager|dev|FCKEditor|old|db|private|members', keyword):
-				print(f'{G}Found {keyword}'.center(terminal_size))
-			else:
-				pass
+		print(f'{LC}robots.txt Found Checking'.center(terminal_size))
+		if re.search('admin|api|controll_panel|users|usarios|administrator|json|login|php|uploads|manager|dev|FCKEditor|old|db|private|members', r.text):
+			for keyword in lines:
+				if re.search('admin|api|controll_panel|users|usarios|administrator|json|login|php|uploads|manager|dev|FCKEditor|old|db|private|members', keyword):
+					print(f'{G}Found {keyword}'.center(terminal_size))
+				else:
+					pass
+		else:
+			print(f'{LR}No interesting files found in robots.txt'.center(terminal_size))
 	else:
-		print(f'{R}[!] Robots.txt was not found. [!]\n'.center(terminal_size))
+		print(f'{R}\033[1m[!] Robots.txt was not found. [!]'.center(terminal_size))
 
 def dirEnum(url, wordlist):
 	try:
-		print(f'{R}Please Wait'.center(terminal_size))
-		print('\n')
+		print(f'{R}\033[1mPlease Wait'.center(terminal_size))
 		for link in open(wordlist).read().splitlines():
+			print(f'Trying {url+link}'.center(terminal_size), end='\r\r')
 			r = session.get(url+link, verify=False, timeout=5, headers=header, allow_redirects=False)
 			if r.status_code == requests.codes.ok:
 				found_list.append(url+link)
@@ -56,7 +57,6 @@ def dirEnum(url, wordlist):
 				not_allowed_list.append(url+link)
 			else:
 				pass
-
 	except requests.exceptions.ConnectionError:
 		logging.critical('Internet is Down')
 		print(f'{R}[!!] Connection Error Please Check Your Internet. [!!]'.center(terminal_size))
@@ -69,17 +69,20 @@ def dirEnum(url, wordlist):
 	except requests.exceptions.ReadTimeout:
 		logging.warning('Try Increasing The timeout')
 		print(f'{LC}[!!] Timeout Error Please Try Increasing The Timeout [!!]'.center(terminal_size))
+	except requests.MaxRetryError:
+		logging.error('Max Retry Error')
+		print(f'{LC}[!!] Max Retry Error Please Try Again. [!!]'.center(terminal_size))
 
 def result(found_list, redirects_list, not_allowed_list):
 	try:
 		for dirs in found_list:
-			print(f'\033[1m{G}Directory/Page:{dirs}		Status:[200]\n')
+			print(f'{G}\033[1m{dirs}		Status:[200]')
 
 		for dirs in not_allowed_list:
-			print(f'\033[1m{R}Directory/Page:{dirs}		Status:[400]\n')
+			print(f'{R}\033[1m{dirs}		Status:[400]')
 
 		for dirs in redirects_list:
-			print(f'\033[1m{LC}Directory/Page:{dirs}	Status:[302]\n')
+			print(f'{LC}\033[1m{dirs}	Status:[302]')
 	except exceptions:
 		pass
 
@@ -101,10 +104,11 @@ def main():
 			print(f'{LR}Starting Script'.center(terminal_size))
 			checkRobots(url)
 			dirEnum(url, wordlist)
+			print('\033[F', end='\033[K')
 			result(found_list, not_allowed_list, redirects_list)
 		except KeyboardInterrupt:
 			logging.debug('You Pressed Something While The Script Is Running')
-			print(f'{R}[!!] Keyboard Interruption [!!]'.center(terminal_size))
+			print(f'{LR}[!!] Keyboard Interruption [!!]'.center(terminal_size))
 		except requests.exceptions.MissingSchema:
 			logging.error('Missing http Schema')
 			print(f'{LC}[!!] Please Add https:// or http:// in your url [!!]'.center(terminal_size))
